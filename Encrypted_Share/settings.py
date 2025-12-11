ae32219233e6b44df6dcef169602a52efe636f97
ChanikyaPonduri <chanikyaponduri22@gmail.com>
2025-12-11 19:32:29 +0530

    Updated settings and requirements for Render

--- Encrypted_Share/settings.py
+++ Encrypted_Share/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-@@b*_+ufkfssg9!n5i7$yt1)@e@s9rm0s3a&@g&*scd3y%x5$*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
